709fb88a71b2e5068710563c0db38503fe44ae5d
Anna <88330648+AnnaSargsyan1@users.noreply.github.com>
2025-09-03 13:17:50 +0400

    Integrated ProductSearch component

--- src/components/products/ProductList.jsx
+++ src/components/products/ProductList.jsx
@@ -4,16 +4,23 @@ import api from "../../api";
 import { CategoryFilter } from "./CategoryFilter";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
+import { ProductSearch } from "./ProductSearch";
 
 export function ProductList() {
     const [books, setBooks] = useState([]);
     const [currentCategory, setCurrentCategory] = useState("All");
     const navigate = useNavigate();
     const [loading, setLoading] = useState(true);
+    const [keyword, setKeyword] = useState("");
     useEffect(() => {
         api.get(`/products/category/${currentCategory}`)
         .then(data => {
-            setBooks(data.data)
+            if (keyword) {
+                const regex = new RegExp(keyword.trim(), "i");
+                setBooks(data.data.filter(book => regex.test(book.name)));
+            } else {
+                setBooks(data.data)
+            }
         })
         .catch(err => {
             if (err.response?.status === 404) {
@@ -22,10 +29,15 @@ export function ProductList() {
                 toast.error(err);
             }
         }).finally(() => setLoading(false));
-    }, [currentCategory]);
+    }, [currentCategory, keyword]);
+
     const handleCategoryFilter = useCallback(category => {
         setCurrentCategory(category || "All");
-    })
+    });
+
+    const handleSearch = keyword => {
+        setKeyword(keyword);
+    }
 
     if (loading) {
         return (
@@ -36,7 +48,8 @@ export function ProductList() {
     }
 
     return <div >
-        <div className="px-6">
+        <div className="px-6 space-y-4">
+            <ProductSearch onSearch={handleSearch} />
             <CategoryFilter onCategoryFilter={handleCategoryFilter} length={books.length} />
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 px-6">
